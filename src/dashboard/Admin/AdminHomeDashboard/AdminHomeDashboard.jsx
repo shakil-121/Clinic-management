e987cba42eab82dd33cc4399d76325757f1f4627
coders-rabbi <pcrabbi2020@gmail.com>
2024-07-08 00:18:59 +0600

    routing error fixed

--- src/dashboard/Admin/AdminHomeDashboard/AdminHomeDashboard.jsx
+++ src/dashboard/Admin/AdminHomeDashboard/AdminHomeDashboard.jsx
@@ -7,7 +7,7 @@ const AdminHomeDashboard = () => {
 
   const Appointments = appointmentData.filter(
     (appointment) =>
-      appointment.status !== "Confirmed" && appointment.status !== "Pending"
+      appointment.status !== "Canceled" && appointment.status !== "Completed"
   );
 
   useEffect(() => {
@@ -22,6 +22,35 @@ const AdminHomeDashboard = () => {
     const status = form.element.status.value;
 
     console.log(status, statusId);
+  };
+
+  const [appointmentId, setAppointmentId] = useState();
+
+  const handleBookClick = (id) => {
+    setAppointmentId(id);
+  };
+
+  const handleStatusUpdate = (status, id) => {
+    console.log(status, id);
+    const statusUpdate = { status };
+
+    fetch(`http://localhost:5000/Appointments/${id}`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(statusUpdate),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.acknowledged) {
+          Swal.fire({
+            title: "Update!",
+            text: "Status update successfully",
+            icon: "success",
+            confirmButtonText: "Ok",
+          });
+        }
+        refetch();
+      });
   };
 
   return (
@@ -45,7 +74,36 @@ const AdminHomeDashboard = () => {
               <td>{data.phone}</td>
               <td>{data.date}</td>
               <td>{data.time}</td>
-              <td>{data.status}</td>
+              <td>
+                <div className="dropdown dropdown-bottom text-black">
+                  <label tabIndex={0} className="">
+                    <button className="btn btn-error">{data.status}</button>
+                  </label>
+                  <ul
+                    tabIndex={0}
+                    className="dropdown-content z-[1] menu p-2 shadow bg-base-100  w-48"
+                  >
+                    <li onClick={() => handleStatusUpdate("Pending", data._id)}>
+                      <a>Pending</a>
+                    </li>
+                    <li
+                      onClick={() => handleStatusUpdate("Confirmed", data._id)}
+                    >
+                      <a>Confirmed</a>
+                    </li>
+                    <li
+                      onClick={() => handleStatusUpdate("Canceled", data._id)}
+                    >
+                      <a>Canceled</a>
+                    </li>
+                    <li
+                      onClick={() => handleStatusUpdate("Completed", data._id)}
+                    >
+                      <a>Completed</a>
+                    </li>
+                  </ul>
+                </div>
+              </td>
             </tr>
           </tbody>
         </table>
